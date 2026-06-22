914dc2cd0904df3038a70cc059583b2e08d99487
HarshitSinha1008 <harshitsinha55@gmail.com>
2026-06-22 21:23:43 +0530

    solved middle of the linked list

--- Linked List/middle_of_the_linked_list.py
+++ Linked List/middle_of_the_linked_list.py
@@ -1,14 +1,11 @@
 class Solution(object):
-    def hasCycle(self, head):
-        
+    def middleNode(self, head):
+
         slow = head
         fast = head
-        
+
         while fast and fast.next is not None:
             slow = slow.next
             fast = fast.next.next
 
-            if slow == fast:
-                return True
-        
-        return False
+        return slow
